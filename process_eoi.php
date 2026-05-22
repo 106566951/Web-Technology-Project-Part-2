ac8991760d5fb8479b0c546e3e5b66f41ecd6712
kaleb-ken <kklarkins06@gmail.com>
2026-05-23 01:05:59 +1000

    Added a data cleansing function for later use. Added all variables for the inputs to the application form.

--- process_eoi.php
+++ process_eoi.php
@@ -1,6 +1,30 @@
 <?php
  if ($_SERVER["REQUEST_METHOD"] == "POST"){
-
+   $job_num = $_POST['']
+   $first_name = $_POST['']
+   $last_name = $_POST['']
+   $DOB = $_POST['']
+   $gender = $_POST['']
+   $street_address = $_POST['']
+   $suburb = $_POST['']
+   $state = $_POST['']
+   $postcode = $_POST['']
+   $email = $_POST['']
+   $mobile = $_POST['']
+   $skills = $_POST['']
+   $extra_skills = $_POST['']
+   
+ 
+   function validate_address(){}
+   
+ 
+ 
+   function clean_data($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+   }
  }  else {
     header("Location: apply.php");
  }
