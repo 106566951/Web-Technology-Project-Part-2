4ea1096ebf9d3833dd6dd35f8ffa675dfe9dc7fc
kaleb-ken <kklarkins06@gmail.com>
2026-05-26 19:23:47 +1000

    Added some documentation to the top of process_eoi page

--- process_eoi.php
+++ process_eoi.php
@@ -1,11 +1,25 @@
+/** 
+*  process_eoi.php
+*
+* - Processes the input of the application form 
+* - Validates the inputs of the form 
+* - Inserts the data into the database.
+*
+* Author:                Kaleb Larkins
+* Date created:          21/5/2026
+* Last Modified:         26/5/2026
+*/
+
 <?php
 require_once "./settings.php";
    # Checks if user came from application form via Post.
  if ($_SERVER["REQUEST_METHOD"] == "POST"){
+   #
    $conn = mysqli_connect($host, $user, "", $sql_db);
    if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
    } else{
+
       # Does a basic clean of the input data
       function clean_data($data) {
          $data = htmlspecialchars(stripslashes(trim($data)));
