74f801ca1b2a076be6753a90556250cb59ae3ced
kaleb-ken <kklarkins06@gmail.com>
2026-05-26 19:50:57 +1000

    Added code that checks if the eoi table exists within the db, then creates a table if it doesn't.

--- process_eoi.php
+++ process_eoi.php
@@ -15,11 +15,40 @@ require_once "./settings.php";
    # Checks if user came from application form via Post.
  if ($_SERVER["REQUEST_METHOD"] == "POST"){
    #
-   $conn = mysqli_connect($host, $user, "", $sql_db);
+   $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
    if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
    } else{
-
+      $table_name = "eoi";
+      $stmt = $conn->prepare("SHOW TABLES LIKE ?"); # A query that checks if a table exists
+      $stmt->bind_param("s", $table_name); # Verifying which table to check for
+      $stmt->execute(); 
+      $result = $stmt->get_result(); # Gaining result from query
+      
+      if ($result->num_rows < 0) { # Checks if table has any rows, if not, Create a table.
+         $create_table = "
+            CREATE TABLE `eoi` (
+            `reference_num` int(6) UNSIGNED NOT NULL,
+            `firstname` varchar(50) NOT NULL,
+            `lastname` varchar(50) NOT NULL,
+            `dob` date NOT NULL,
+            `gender` enum('other','Male','Female') NOT NULL DEFAULT 'other',
+            `street_address` text NOT NULL,
+            `suburb_town` varchar(100) NOT NULL,
+            `state` enum('VIC','NSW','QLD','NT','WA','SA','TAS','ACT') NOT NULL,
+            `postcode` int(4) UNSIGNED NOT NULL,
+            `email` varchar(100) NOT NULL,
+            `phone_num` int(10) UNSIGNED ZEROFILL NOT NULL,
+            `skill_set` set('Communication','Consultation Strategy Design','Frontend development','Backend development','Knowledge on Git version control') NOT NULL,
+            `other_skills` text NOT NULL,
+            `status` enum('New','Current','Final','') NOT NULL DEFAULT 'New'
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+         ";
+         if(mysqli_query($conn, $create_table)){
+            echo "Created Table!";
+         }
+      }
+         
       # Does a basic clean of the input data
       function clean_data($data) {
          $data = htmlspecialchars(stripslashes(trim($data)));
