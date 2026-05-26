d323a6ae69a4eb6d59eb13cffa0f60781c2d533f
Leo Dalton <Leoician@gmail.com>
2026-05-26 23:07:40 +1000

    small changes to about page and manage

---
+++ manage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="The Coders" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log-in Page</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <style></style>
+</head>
+<body>
+    
+</body>
+</html>
