bcfbe5651c9e09e9e6ba3f575c2b9529faf9f0fe
Chittuparamban <joshuajoshipvt@gmail.com>
2026-05-28 13:32:35 +1000

    clean up 2

---
+++ greengrid1_db.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 24, 2026 at 09:38 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `greengrid_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eoi`
+--
+
+CREATE TABLE `eoi` (
+  `EOI_ID` INT AUTO_INCREMENT PRIMARY KEY,
+  `reference_num` int(6) UNSIGNED NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `dob` date NOT NULL,
+  `gender` enum('other','Male','Female') NOT NULL DEFAULT 'other',
+  `street_address` text NOT NULL,
+  `suburb_town` varchar(100) NOT NULL,
+  `state` enum('VIC','NSW','QLD','NT','WA','SA','TAS','ACT') NOT NULL,
+  `postcode` int(4) UNSIGNED NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone_num` int(10) UNSIGNED ZEROFILL NOT NULL,
+  `skill_set` set('Communication','Consultation Strategy Design','Frontend development','Backend development','Knowledge on Git version control') NOT NULL,
+  `other_skills` text NOT NULL,
+  `status` enum('New','Current','Final','') NOT NULL DEFAULT 'New'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE jobs (
+    reference_num VARCHAR(10) NOT NULL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    badge VARCHAR(100),
+    description TEXT,
+    salary VARCHAR(50),
+    reports_to VARCHAR(100),
+    responsibilities TEXT,
+    essential_requirements TEXT,
+    preferred_requirements TEXT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member_contributions`
+--
+
+CREATE TABLE `member_contributions` (
+  `id` int(6) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `shared_responsibility` text NOT NULL,
+  `individual_contributions` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `eoi`
+--
+ALTER TABLE `eoi` ADD FULLTEXT KEY `street_address` (`street_address`);
+ALTER TABLE `eoi` ADD FULLTEXT KEY `suburb_town` (`suburb_town`);
+
+--
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`reference_num`);
+
+--
+-- Indexes for table `member_contributions`
+--
+ALTER TABLE `member_contributions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `member_contributions`
+--
+ALTER TABLE `member_contributions`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
