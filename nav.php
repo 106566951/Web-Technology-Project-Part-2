df12fe1434a8a3153a5e2c79bd85eb24ad52b86d
Chittuparamban <joshuajoshipvt@gmail.com>
2026-05-28 13:06:36 +1000

    Fix navigation link to point to about.php instead of about.html

--- nav.php
+++ nav.php
@@ -6,6 +6,6 @@
             <li><a class="navlink" href="index.php">Home Page</a></li>
             <li><a class="navlink" href="apply.php">Apply Now</a></li>
             <li><a class="navlink" href="jobs.php">Job Listings</a></li>
-            <li><a class="navlink" href="about.html">About Us</a></li>
+            <li><a class="navlink" href="about.php">About Us</a></li>
         </ul>
     </nav>
