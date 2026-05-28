1d1c67ee3c526d7f885c3aa82177fb2faa292809
Leo Dalton <Leoician@gmail.com>
2026-05-28 17:25:33 +1000

    moved proccess_login to login because I accsidently put it in manage.php

--- manage.php
+++ manage.php
@@ -33,33 +33,4 @@
     </div>
 </main>
 
-
-<?php  
-
-    // Takes user to manage.php if they are the right user
-    session_start();
-
-    /* Use `$_POST` to capture the username and password from the form. */
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    
-    /*  If theusername is correct 'YourName' and the password is 'YourStudentID' */
-    if ($username == 'admin' && $password == 'admin') {
-
-        /* set asession variable `$_SESSION['user']` to store the username.*/
-        $_SESSION['user'] = $username;
-
-        /* Redirect to welcome.php if login succeeds; */
-        header('Location: manage.php');
-
-    } 
-    else {
-
-        /* otherwise return to login.php with an error. */
-        header('Location: login.php');
-
-    }
-
-?>
-
 <?php include 'footer.inc'; ?>
